669aa51f230c24e2637310faf86fe0b3dbe88db0
egor187 <py.egor.py@gmail.com>
2021-12-20 21:42:30 +0300

    apply RemoteDriver in main() instead local

--- app/run.py
+++ app/run.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python3
 
-import sys
-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
-from bs4 import BeautifulSoup
-
-from driver import LocalWBDriver
-from parser import parse_currency_html
+from driver import RemoteWBDriver, WBDriver
+from parser import get_currency_mapper
 
 
 def main():
     # currency_code = sys.argv[1]
     currency_code = "R01010"  # TODO fix hardcoded currency code
-    currency_html = LocalWBDriver(currency_code=currency_code).get_currency_html()
-    currency_data = parse_currency_html(currency_html=currency_html)
+    # currency_html = WBDriver(currency_code=currency_code).get_currency_html()
+    currency_html = RemoteWBDriver(currency_code=currency_code).get_currency_html()
+    # currency_data = parse_currency_html(currency_html=currency_html)
+    currency_list = get_currency_mapper(currency_html=currency_html)
 
 
 if __name__ == "__main__":
