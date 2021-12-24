9bddea0f7b48af0334268e709c72cc0005d753e0
egor187 <py.egor.py@gmail.com>
2021-12-24 13:56:47 +0300

    apply an "input" for user to provide currency name

--- app/run.py
+++ app/run.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
-from driver import RemoteWBDriver, WBDriver
-from parser import get_currency_mapper
+from app.driver import RemoteWBDriver, WBDriver
+from app.parser import populate_db_with_currencies, parse_currency_html
+from app.db import get_currency_code_by_name
+from app.exc import DataNotFound
 
 
 def main():
-    # currency_code = sys.argv[1]
-    currency_code = "R01010"  # TODO fix hardcoded currency code
-    # currency_html = WBDriver(currency_code=currency_code).get_currency_html()
-    currency_html = RemoteWBDriver(currency_code=currency_code).get_currency_html()
-    # currency_data = parse_currency_html(currency_html=currency_html)
-    currency_list = get_currency_mapper(currency_html=currency_html)
+    # currency_code = sys.argv[1]  #TODO try to apply instead user input
+    flag = True
+
+    while flag:
+        currency_name = input("Provide a currency name: ")
+
+        try:
+            currency_code = get_currency_code_by_name(currency_name=currency_name)
+        except DataNotFound:
+            print("No such currency founded")
+        else:
+            flag = False
+            currency_html = WBDriver(currency_code=currency_code).get_currency_html()
+            # currency_html = RemoteWBDriver(currency_code=currency_code).get_currency_html()
+            currency_data = parse_currency_html(currency_html=currency_html)
+            populate_db_with_currencies(currency_html=currency_html)
 
 
 if __name__ == "__main__":
