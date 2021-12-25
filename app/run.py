fcec7af4b43804816478ec185dd700d93bf41c01
egor187 <py.egor.py@gmail.com>
2021-12-25 12:12:23 +0300

    use sys.argv instead input() to pass currency name

--- app/run.py
+++ app/run.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import os
+import sys
 
 from driver import RemoteWBDriver, WBDriver
 from parser import populate_db_with_currencies, parse_currency_html
@@ -7,12 +9,11 @@ from exc import DataNotFound
 
 
 def main():
-    # currency_code = sys.argv[1]  #TODO try to apply instead user input
     flag = True
 
     while flag:
-        currency_name = input("Provide a currency name: ")
-
+        currency_name = sys.argv[1]
+        # currency_name = input("Provide a currency name: ")
         try:
             currency_code = get_currency_code_by_name(currency_name=currency_name)
         except DataNotFound:
@@ -23,6 +24,7 @@ def main():
             currency_html = RemoteWBDriver(currency_code=currency_code).get_currency_html()
             currency_data = parse_currency_html(currency_html=currency_html)
             populate_db_with_currencies(currency_html=currency_html)
+            print(currency_data)  # debug print
 
 
 if __name__ == "__main__":
