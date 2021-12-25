b9b0567516e521efee69f01d732561e0cbb31445
egor187 <py.egor.py@gmail.com>
2021-12-25 15:52:27 +0300

    added helpers for retrieve currency id by code and method for add currency data to db

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import create_engine, select, inspect
 from sqlalchemy.orm import Session
 from dotenv import load_dotenv, find_dotenv
-from schema import Base, Currency
+from schema import Base, Currency, CurrencyData
 from exc import DataNotFound
 
 load_dotenv(find_dotenv())
@@ -30,6 +30,29 @@ def get_currency_code_by_name(currency_name: str):
     if not currency_code:
         raise DataNotFound
     return currency_code[0]
+
+
+def get_currency_id_by_code(currency_code: str):
+    with session:
+        currency_id = session.execute(
+            select(Currency.id).where(Currency.code == currency_code)
+        ).fetchone()
+    if not currency_id:
+        raise DataNotFound
+    return currency_id[0]
+
+
+def add_currency_data(currency_code: str, data: dict) -> None:
+    with session:
+        try:
+            currency_id = get_currency_id_by_code(currency_code)
+        except DataNotFound:
+            print("No such currency code")
+        else:
+            for key in data.keys():
+                obj = CurrencyData(currency_id=currency_id, date=key, value=data[key])
+                session.add(obj)
+        session.commit()
 
 
 def _check_db():
